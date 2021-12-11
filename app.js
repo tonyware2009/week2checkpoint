c878070e7de01a59b26742a76aee56576c0851df
Tony Ware <ton8675309@gmail.com>
2021-12-10 17:27:35 -0700

    almost finished

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const clickUpgrades = {
     multiplier: 1
   },
   HeliumDrillHardened: {
-    price: 100,
+    price: 250,
     quantity: 0,
     multiplier: 2
   }
@@ -28,10 +28,69 @@ let automaticUpgrades = {
 
 function harvest() {
   helium++
+  helium += clickUpgrades.HeliumDrill.quantity * clickUpgrades.HeliumDrill.multiplier
+  helium += clickUpgrades.HeliumDrillHardened.quantity * clickUpgrades.HeliumDrillHardened.multiplier
+
   // console.log(helium)
   update()
+
 }
 
 function update() {
   document.getElementById('heliumAmount').innerHTML = helium
 }
+
+function helDrill() {
+  if (helium >= clickUpgrades.HeliumDrill.price) {
+    clickUpgrades.HeliumDrill.quantity++
+    helium -= clickUpgrades.HeliumDrill.price
+    clickUpgrades.HeliumDrillHardened.price *= 2
+    update()
+  }
+
+  document.getElementById('heliumTool').innerHTML = clickUpgrades.HeliumDrill.quantity
+
+
+}
+function heliumHardened() {
+  if (helium >= clickUpgrades.HeliumDrillHardened.price) {
+    clickUpgrades.HeliumDrillHardened.quantity++
+    helium -= clickUpgrades.HeliumDrillHardened.price
+    clickUpgrades.HeliumDrillHardened.price *= 2
+
+    update()
+  }
+  document.getElementById('heliumHD').innerHTML = clickUpgrades.HeliumDrillHardened.quantity
+}
+
+
+
+function helGen() {
+
+  if (helium >= automaticUpgrades.HeliumGenerator.price) {
+    automaticUpgrades.HeliumGenerator.quantity++
+    helium -= automaticUpgrades.HeliumGenerator.price
+    automaticUpgrades.HeliumGenerator.price *= 2
+    update()
+  }
+  document.getElementById('genUp').innerHTML = automaticUpgrades.HeliumGenerator.quantity
+}
+
+
+
+function SolarG() {
+  if (helium >= automaticUpgrades.SolarGenerator.price) {
+    automaticUpgrades.SolarGenerator.quantity++
+    helium -= automaticUpgrades.SolarGenerator.price
+    automaticUpgrades.SolarGenerator.price *= 2
+    update()
+    document.getElementById('SgenUp').innerHTML = helium
+  }
+}
+function collecautoupgrades() {
+  helium += automaticUpgrades.HeliumGenerator.quantity * automaticUpgrades.HeliumGenerator.multiplier
+  helium += automaticUpgrades.SolarGenerator.quantity * automaticUpgrades.SolarGenerator.multiplier
+  console.log('collecting');
+  update()
+}
+setInterval(collecautoupgrades, 3000)
